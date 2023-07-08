0a0108fba8eb7a1fe8fb6b5f022a30de73939cd8
lastZu <cdrom2015@ya.ru>
2023-07-09 00:33:16 +0300

    first commit

---
+++ src/main/java/EnrichmentService.java
@@ -0,0 +1,10 @@
+public class EnrichmentService {
+    // возвращается обогащенный (или необогащенный content сообщения)
+    public static void main(String[] args) {
+
+    }
+
+    public String enrich(Message message) {
+        return "";
+    }
+}
